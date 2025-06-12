ddc4574c9bdb0ba3349310de45bdfff263902311
Vatsal Deepak Rathi <ds099315@gmail.com>
2025-06-12 15:25:54 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -2,30 +2,33 @@
 
 import dotenv from "dotenv"
 
+import express from "express"
 import mongoose from"mongoose"
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
 
-
+const app=express()
 dotenv.config({
     path:'./env'
 })
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , () =>{
+        console.log(`server is running at port ${process.env.PORT}`);
+        
+    })
+})
+.catch((error)=>{
+    console.log("Mongo DB connection failed");
+})
 
 
 
-
-
-
-
-
-
-
-
-
+// one of the method to connect database in mern stack project 
 
 /*
 import express from "express"
+const app=express()
 
 ;(async () => {
     try {
@@ -33,7 +36,6 @@ import express from "express"
     app.on("error" ,(error) => {
         console.log("ERR : " , error);
         throw error
-        
     })
     app.listen(process.env.PORT,()=>{
         console.log(`App is listening on port ${process.env.PORT}`);
@@ -41,7 +43,7 @@ import express from "express"
     })
     } catch (error) {
         console.log("Error");
-        throw err
+        throw error
     }
 })()   // iife function
 */
